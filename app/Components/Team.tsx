bd5940f55c62373cd71f7a868d301279ca1f3896
MasterK <keshavagarwal12325@gmail.com>
2023-09-12 00:40:05 +0530

    update

--- app/Components/Team.tsx
+++ app/Components/Team.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import styles from '../styles/alumniComponents.module.css'
+import styles from '../styles/teamComponents.module.css'
 import Image from 'next/image';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 interface Team {
     id: number;
@@ -13,25 +16,47 @@ interface TeamProps {
     teamData: Team[];
 }
 
-const TeamComponent: React.FC<TeamProps> = ({ teamData }) => {
-    return (
-        <div>
-            <h2 className={styles.heading}>Our Team</h2>
-            <ul className={styles['alumni-list']}>
-                {teamData.map((team) => (
-                    <li key={team.id} className={styles['alumni-item']}>
-                        <div className={styles["dpeDzF"]}>
-                            <div className={styles["content"]}>
-                                <Image src={team.src} alt="alumni" width={200} height={200} className={styles['alumniImage']}/>
-                                <h3 className={styles['alumni-name']}> {team.name}</h3>
-                                <div className={styles['alumni-position']}><div className={styles['circle']}></div>{team.position}</div>
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+const CarouselTeam: React.FC<TeamProps> = ({ teamData }) => {
+
+    const windowWidth = window.innerWidth;
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: windowWidth > 1000 ? 3 : 1,
+        slidesToScroll: 2,
+        pauseOnFocus: true,
+        autoplay: true,
+        autoplaySpeed: 1400
+        };
+        return (
+          <div className={styles["alumni-carousel"]}>
+            <Slider {...settings}>
+            {teamData.map((team) => (
+                      <div key={team.id} className={styles['alumni-item']}>
+                          <div className={styles["dpeDzF"]}>
+                              <div className={styles["content"]}>
+                                  <Image src={team.src} alt="alumni" width={200} height={200} className={styles['alumniImage']}/>
+                                  <h3 className={styles['alumni-name']}> {team.name}</h3>
+                                  <div className={styles['alumni-position']}><div className={styles['circle']}></div>{team.position}</div>
+                              </div>
+                          </div>
+                      </div>
+                  ))}
+          </Slider>
+           </div>
+        );
+  }
+  
+  
+  const TeamComponent: React.FC<TeamProps> = ({ teamData }) => {
+      return (
+          <div>
+              <h2 className={styles.heading}>Our Team</h2>
+              <CarouselTeam teamData={teamData} />
+          </div>
+      );
+  };
 
 export default TeamComponent;

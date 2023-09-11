0f89e201ed28c4f02cadb36d15d7b810e340c87e
MasterK <keshavagarwal12325@gmail.com>
2023-09-12 02:01:29 +0530

    yt

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
             <iframe
               src="https://www.youtube.com/watch?v=MSTDhx574P4"
               title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allow="accelerometer; autoplay; clipboard-write; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
           </motion.div>

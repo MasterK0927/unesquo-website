f678015ac0641cb676072c5630c1a1cfc4ba3aaf
crap1nyk <nv06645@gmail.com>
2023-12-14 13:14:36 +0530

    committed

--- app/bitmun/page.tsx
+++ app/bitmun/page.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import NavBar from '../Components/NavBar'
-import Footer from '../Components/Footer'
-import Chairs from '../Components/bitmun/OurTeam/ChairsComponent'
-
+"use client"
+import React from 'react';
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+import FourGrids from '../Components/bitmun/OurTeam/FourGrids';
+import Timer from '../Components/bitmun/OurTeam/Timer';
 
 const Bitmun = () => {
+  const initialTime = 300;
+
   return (
     <div>
-        <NavBar />
-        <Chairs />
-        <Footer />
+      <NavBar />
+      <Timer initialTime={initialTime} />
+      <FourGrids />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Bitmun
+export default Bitmun;

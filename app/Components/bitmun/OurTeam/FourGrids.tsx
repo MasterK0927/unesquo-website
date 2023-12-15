6e50d6500f60565e3ba43d57d0b739d35fcf0928
MasterK <keshavagarwal12325@gmail.com>
2023-12-15 13:16:03 +0530

    fixed and stylled the timer component

--- app/Components/bitmun/OurTeam/FourGrids.tsx
+++ app/Components/bitmun/OurTeam/FourGrids.tsx
@@ -6,8 +6,7 @@ interface GridProps {
 }
 
 const Grid: React.FC<GridProps> = ({ content, imageUrl }) => {
-
-  const gridStyles = {
+  const gridStyles: React.CSSProperties = {
     border: '1px solid #ccc',
     padding: '10px',
     margin: '5px',
@@ -15,18 +14,24 @@ const Grid: React.FC<GridProps> = ({ content, imageUrl }) => {
     alignItems: 'center',
     display: 'flex',
     flexDirection: 'column',
-    width: imageUrl ? 'auto' : '70vh', 
-    height: imageUrl ? 'auto' : '70vh', 
+    width: imageUrl ? 'auto' : '70vh',
+    height: imageUrl ? 'auto' : '70vh',
   };
 
-  const textStyles = {
-    textAlign: 'center', 
+  const textStyles: React.CSSProperties = {
+    textAlign: 'center',
     fontSize: '25px',
   };
 
   return (
     <div style={gridStyles}>
-      {imageUrl && <img src={imageUrl} alt="Grid Image" style={{ maxWidth: '100%', maxHeight: '70vh', objectFit: 'cover', marginBottom: '10px' }} />}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt="Grid Image"
+          style={{ maxWidth: '100%', maxHeight: '70vh', objectFit: 'cover', marginBottom: '10px' }}
+        />
+      )}
       <p style={textStyles}>{content}</p>
     </div>
   );
@@ -35,16 +40,16 @@ const Grid: React.FC<GridProps> = ({ content, imageUrl }) => {
 const FourGridsSection: React.FC = () => {
   return (
     <div>
-      <h2>Four Grids Section</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         <Grid content="Grid 1" imageUrl="images/rover.jpg" />
-  
-        <Grid content="Welcome to BITMUN, an immersive Model United Nations experience that delves into the heart of international diplomacy. BITMUN provides a platform for students to engage in diplomatic discourse, foster critical thinking, and develop a nuanced understanding of global issues." style={{ width: '40vh', height: '40vh' }} />
-        <Grid content="Grid 3" imageUrl="images/rover.jpg" />
-        
+
         <Grid
           content="Welcome to BITMUN, an immersive Model United Nations experience that delves into the heart of international diplomacy. BITMUN provides a platform for students to engage in diplomatic discourse, foster critical thinking, and develop a nuanced understanding of global issues."
-          style={{ width: '40vh', height: '40vh' }}
+        />
+        <Grid content="Grid 3" imageUrl="images/rover.jpg" />
+
+        <Grid
+          content="Welcome to BITMUN, an immersive Model United Nations experience that delves into the heart of international diplomacy. BITMUN provides a platform for students to engage in diplomatic discourse, foster critical thinking, and develop a nuanced understanding of global issues."
         />
       </div>
     </div>
--- app/Components/bitmun/OurTeam/Timer.tsx
+++ app/Components/bitmun/OurTeam/Timer.tsx
@@ -27,16 +27,106 @@ const Timer: React.FC<TimerProps> = ({ initialTime }) => {
     const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = seconds % 60;
 
-    return `${String(days).padStart(2, '0')}d ${String(hours).padStart(2, '0')}h ${String(minutes).padStart(2, '0')}m ${String(remainingSeconds).padStart(2, '0')}s`;
+    return (
+      <div className="timer-items">
+        <div className="timer-item">{String(days).padStart(2, '0')}d</div>
+        <div className="timer-item">{String(hours).padStart(2, '0')}h</div>
+        <div className="timer-item">{String(minutes).padStart(2, '0')}m</div>
+        <div className="timer-item">{String(remainingSeconds).padStart(2, '0')}s</div>
+        <style jsx>
+          {`
+            .timer-items {
+              display: flex;
+              flex-direction: row;
+              justify-content: center;
+              align-items: center;
+              gap: 10px;
+            }
+            .timer-item {
+              font-size: 3em;
+              font-weight: semibold;
+              padding: 5px;
+              border-radius: 5px;
+              margin: 5px;
+              display: flex;
+              box-shadow: 2px 2px 10px red, -2px -2px 10px red, 2px -2px 10px red, -2px 2px 10px rgba(5, 5, 0, 0.1), 0 0 10px rgba(5, 5, 0, 0.1), inset 0 0 10px rgba(5, 5, 0, 0.1);
+              background: rgba(192, 192, 192, 0.2);
+              backdrop-filter: blur(10px);
+            }
+    
+            @media (max-width: 1000px) {
+              .timer-item {
+                font-size: 1.75em;
+                font-weight: semibold;
+                padding: 5px;
+                border-radius: 5px;
+                margin: 5px;
+                display: flex;
+                box-shadow: 2px 2px 10px red, -2px -2px 10px red, 2px -2px 10px red, -2px 2px 10px rgba(5, 5, 0, 0.1), 0 0 10px rgba(5, 5, 0, 0.1), inset 0 0 10px rgba(5, 5, 0, 0.1);
+                background: rgba(192, 192, 192, 0.2);
+                backdrop-filter: blur(10px);
+              }
+    
+              .timer-item {
+                margin: 0 5px;
+              }
+            }
+          `}
+        </style>
+      </div>
+    );
   };
 
   return (
-    <div style={{ textAlign: 'center', margin: '5px', border: '1px solid white', borderRadius: '5px', padding: '10px' }}>
-      <h2 style={{ fontFamily: 'Serif', color: '#E4931D', fontSize: '5em' }}>BITMUN 24'</h2>
-      <h6 style={{ fontFamily: 'Serif', color: 'white', fontSize: '2em' }}>Overcoming Differences</h6>
-      <div style={{ fontFamily: 'Serif', fontSize: '3em', fontWeight: 'semibold', padding: '5px', borderRadius: '5px', marginTop: '5px', display: 'inline-block', boxShadow: '0 0 10px rgba(5, 5, 0, 0.1)' }}>
-        {formatTime(time)}
-      </div>
+    <div className="timer-container">
+      <h2 className="timer-heading">BITMUN 24'</h2>
+      <h6 className="timer-subheading">Overcoming Differences</h6>
+      {formatTime(time)}
+      <style jsx>{`
+        .timer-container {
+          text-align: center;
+          margin: 5px;
+          border-radius: 15px;
+          padding: 20px 20px 40px 20px;
+          background: linear-gradient(180deg, rgba(255, 255, 255, 0.2) 0%, rgba(255, 255, 255, 0.05) 100%);
+          backdrop-filter: blur(10px);
+          box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .timer-heading {
+          font-family: 'Calibri', sans-serif;
+          color: #E4931D;
+          font-size: 5em;
+          margin-bottom: 10px;
+        }
+
+        .timer-subheading {
+          font-family: 'Calibri', sans-serif;
+          color: white;
+          font-size: 2em;
+          margin-bottom: 20px;
+        }
+
+        @media (max-width: 1000px) {
+          .timer-heading {
+            font-family: 'Calibri', sans-serif;
+            color: #E4931D;
+            font-size: 3.75em;
+            margin-bottom: 10px;
+          }
+
+          .timer-subheading {
+            font-family: 'Calibri', sans-serif;
+            color: white;
+            font-size: 1.75em;
+            margin-bottom: 20px;
+          }
+        }
+      `}</style>
     </div>
   );
 };

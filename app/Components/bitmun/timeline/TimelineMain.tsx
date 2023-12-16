bb37e096adc89659ddf57400df30e86f416aa463
MasterK <keshavagarwal12325@gmail.com>
2023-12-16 15:13:59 +0530

    removed unwanted timeline component

--- app/Components/bitmun/timeline/TimelineMain.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import VerticalTimeline from './TimelineComponent';
-import { events } from './Events';
-
-function App() {
-  return <VerticalTimeline events={events} interval={20000} />;
-}
-
-export default App;

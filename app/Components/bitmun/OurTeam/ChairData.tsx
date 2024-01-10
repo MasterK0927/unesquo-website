eca71411647b8a271dd10e58517ec852c41b381a
MasterK <keshavagarwal12325@gmail.com>
2024-01-10 11:47:18 +0530

    added

--- app/Components/bitmun/OurTeam/ChairData.tsx
+++ app/Components/bitmun/OurTeam/ChairData.tsx
@@ -9,20 +9,20 @@ import Press from './press';
 const ungaData = [
   {
     id: 1,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/chair/Aishnit_Yadav.jpg',
+    name: 'Aishnit Yadav',
     position: 'CHAIR',
   },
   {
     id: 2,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/chair/Diptesh_Banerjee.jpg',
+    name: 'Diptesh Banerjee',
     position: 'VICECHAIR',
   },
   {
     id: 3,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/images/webp/Shaurya.webp',
+    name: 'Shaurya Singh',
     position: 'RAPPEUTER',
   },
 ];
@@ -30,81 +30,75 @@ const ungaData = [
 const warCabData = [
   {
     id: 1,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/chair/Pragyan_Sharma.jpeg',
+    name: 'Pragyan Sharma',
     position: 'CHAIR',
   },
   {
     id: 2,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/chair/Akshat_Raj_Sharma.jpg',
+    name: 'Akshat Raj Sharma',
     position: 'VICECHAIR',
-  },
-  {
-    id: 3,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'Rappeuter',
   },
 ];
 const MomData = [
   {
     id: 1,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/images/webp/NikhilVerma.webp',
+    name: 'Nikhil Verma',
     position: 'CHAIR',
   },
   {
     id: 2,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
+    src: '/images/webp/Aayushii.webp',
+    name: 'Aayushii Singh',
     position: 'VICECHAIR',
   },
   {
     id: 3,
     src: '/images/webp/tba.webp',
-    name: 'TBA',
+    name: 'Siddhant Jagat',
     position: 'Rappeuter',
   },
 ];
 const jsipData = [
   {
     id: 1,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'CHAIR',
+    src: '/chair/Avyakt_Mishra.jpg',
+    name: 'Avyakt Mishra',
+    position: 'SPEAKER',
   },
   {
     id: 2,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'VICECHAIR',
-  },
-  {
-    id: 3,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'Rappeuter',
+    src: '/images/webp/Ambuj.webp',
+    name: 'Ambuj Mishra',
+    position: 'DEPUTY SPEAKER',
   },
 ];
 const pressData = [
   {
     id: 1,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'Secretary General',
+    src: '/chair/Aditya_Dubey.jpg',
+    name: 'Aditya Dubey',
+    position: 'HEAD',
   },
   {
     id: 2,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'Deputy Secretary General',
+    src: '/images/webp/Khushi.webp',
+    name: 'Khushi Mandal',
+    position: 'EDITOR-IN-CHIEF',
   },
   {
     id: 3,
-    src: '/images/webp/tba.webp',
-    name: 'TBA',
-    position: 'Deputy Secretary General',
+    src: '/images/webp/Amiya.webp',
+    name: 'Amiya Jha',
+    position: 'PHOTOGRAPHER-IN-CHIEF',
+  },
+  {
+    id: 4,
+    src: '/images/webp/HarshN.webp',
+    name: 'Harsh Narayan',
+    position: 'HEAD CARICATURIST',
   },
 ];
 

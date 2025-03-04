2d991397dc05c137636f2c24b8fd93cb366a6664
Mrityunjay Raj <mr.raj.earth@gmail.com>
2025-03-04 23:58:47 +0530

    collaborators page added

--- app/Components/bitmun/OurTeam/CollaboratorsData.tsx
+++ app/Components/bitmun/OurTeam/CollaboratorsData.tsx
@@ -5,31 +5,46 @@ import Collaborators from '../collaborations/CollaborationsStr';
 const collaboratorsData = [
   {
     id: 1,
+    src: '/Collaborations/2025/dublieu_com.jpg',
+    name: 'Dublieu',
+  },
+  {
+    id: 2,
+    src: '/Collaborations/2025/VJMUN.jpg',
+    name: 'VJMUN',
+  },
+  {
+    id: 3,
+    src: '/Collaborations/2025/RHETORICA.jpg',
+    name: 'Rhetorica',
+  },
+  {
+    id: 4,
     src: '/Collaborations/assembly.png',
     name: 'Assembly of Nations',
   },
   {
-    id: 2,
+    id: 5,
     src: '/Collaborations/juds.jpeg',
     name: 'JUDS',
   },
   {
-    id: 3,
+    id: 6,
     src: '/Collaborations/RTU.jpeg',
     name: 'RTU MUN',
   },
   {
-    id: 4,
+    id: 7,
     src: '/Collaborations/ssuimun2.jpeg',
     name: 'SSUIMUN',
   },
   {
-    id: 5,
+    id: 8,
     src: '/Collaborations/bbsrmun.jpeg',
     name: 'IIT-BBSR MUN',
   },
   {
-    id: 6,
+    id: 9,
     src: '/Collaborations/kgp.jpeg',
     name: 'GMUN IIT-KGP',
   },
